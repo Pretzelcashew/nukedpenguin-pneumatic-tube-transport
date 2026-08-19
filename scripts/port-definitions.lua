6d2dd01368af52852613f72fd37873b11a98bf2e
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-08-19 11:59:19 -0400

    more port definitions refinements

--- scripts/port-definitions.lua
+++ scripts/port-definitions.lua
@@ -3,76 +3,70 @@ local port_definitions = {
 
     -- FIXED HUBS
     ["capsule-hub-horizontal"] = {
-        ports = {
-            { offset = {x = -0.5, y = -0.5} },
-            { offset = {x =  0.5, y = -0.5} },
-            { offset = {x = -0.5, y =  0.5} },
-            { offset = {x =  0.5, y =  0.5} },
-            { offset = {x = -1.0, y =  0.0} },
-            { offset = {x =  1.0, y =  0.0} }
+        [defines.direction.north] = {
+            { flow = "any", connection = "join", offset = {x = -0.5, y = -0.5} },
+            { flow = "any", connection = "join", offset = {x =  0.5, y = -0.5} },
+            { flow = "any", connection = "join", offset = {x = -0.5, y =  0.5} },
+            { flow = "any", connection = "join", offset = {x =  0.5, y =  0.5} },
+            { flow = "any", connection = "join", offset = {x = -1.0, y =  0.0} },
+            { flow = "any", connection = "join", offset = {x =  1.0, y =  0.0} }
         }
     },
 
     ["capsule-hub-vertical"] = {
-        ports = {
-            { offset = {x =  0.0, y = -1.0} },
-            { offset = {x =  0.0, y =  1.0} },
-            { offset = {x = -0.5, y = -0.5} },
-            { offset = {x = -0.5, y =  0.5} },
-            { offset = {x =  0.5, y = -0.5} },
-            { offset = {x =  0.5, y =  0.5} }
+        [defines.direction.north] = {
+            { flow = "any", connection = "join", offset = {x =  0.0, y = -1.0} },
+            { flow = "any", connection = "join", offset = {x =  0.0, y =  1.0} },
+            { flow = "any", connection = "join", offset = {x = -0.5, y = -0.5} },
+            { flow = "any", connection = "join", offset = {x = -0.5, y =  0.5} },
+            { flow = "any", connection = "join", offset = {x =  0.5, y = -0.5} },
+            { flow = "any", connection = "join", offset = {x =  0.5, y =  0.5} }
         }
     },
 
-    -- ROTATABLE TUBES (Bi-directional ports)
+    -- ROTATABLE TUBES
     ["pneumatic-tube"] = {
-        by_direction = {
-            [defines.direction.north] = {
-                { offset = {x = 0.0, y = -1.0} },
-                { offset = {x = 0.0, y =  1.0} }
-            },
-            [defines.direction.south] = {
-                { offset = {x = 0.0, y = -1.0} },
-                { offset = {x = 0.0, y =  1.0} }
-            },
-            [defines.direction.east] = {
-                { offset = {x = -1.0, y = 0.0} },
-                { offset = {x =  1.0, y = 0.0} }
-            },
-            [defines.direction.west] = {
-                { offset = {x = -1.0, y = 0.0} },
-                { offset = {x =  1.0, y = 0.0} }
-            }
+        [defines.direction.north] = {
+            { flow = "any", connection = "merge", offset = {x = 0.0, y = -1.0} },
+            { flow = "any", connection = "merge", offset = {x = 0.0, y =  1.0} }
+        },
+        [defines.direction.south] = {
+            { flow = "any", connection = "merge", offset = {x = 0.0, y = -1.0} },
+            { flow = "any", connection = "merge", offset = {x = 0.0, y =  1.0} }
+        },
+        [defines.direction.east] = {
+            { flow = "any", connection = "merge", offset = {x = -1.0, y = 0.0} },
+            { flow = "any", connection = "merge", offset = {x =  1.0, y = 0.0} }
+        },
+        [defines.direction.west] = {
+            { flow = "any", connection = "merge", offset = {x = -1.0, y = 0.0} },
+            { flow = "any", connection = "merge", offset = {x =  1.0, y = 0.0} }
         }
     },
 
-    -- ROTATABLE PUMPS (Input vs. Output flow)
+    -- ROTATABLE PUMPS
     ["pneumatic-pump"] = {
-        by_direction = {
-            [defines.direction.north] = {
-                { type = "input",  offset = {x = 0.0, y =  1.0} },
-                { type = "output", offset = {x = 0.0, y = -1.0} }
-            },
-            [defines.direction.east] = {
-                { type = "input",  offset = {x = -1.0, y = 0.0} },
-                { type = "output", offset = {x =  1.0, y = 0.0} }
-            },
-            [defines.direction.south] = {
-                { type = "input",  offset = {x = 0.0, y = -1.0} },
-                { type = "output", offset = {x = 0.0, y =  1.0} }
-            },
-            [defines.direction.west] = {
-                { type = "input",  offset = {x =  1.0, y = 0.0} },
-                { type = "output", offset = {x = -1.0, y = 0.0} }
-            }
+        [defines.direction.north] = {
+            { flow = "in",  connection = "join", offset = {x = 0.0, y =  1.0} },
+            { flow = "out", connection = "join", offset = {x = 0.0, y = -1.0} }
+        },
+        [defines.direction.east] = {
+            { flow = "in",  connection = "join", offset = {x = -1.0, y = 0.0} },
+            { flow = "out", connection = "join", offset = {x =  1.0, y = 0.0} }
+        },
+        [defines.direction.south] = {
+            { flow = "in",  connection = "join", offset = {x = 0.0, y = -1.0} },
+            { flow = "out", connection = "join", offset = {x = 0.0, y =  1.0} }
+        },
+        [defines.direction.west] = {
+            { flow = "in",  connection = "join", offset = {x =  1.0, y = 0.0} },
+            { flow = "out", connection = "join", offset = {x = -1.0, y = 0.0} }
         }
     }
 }
 
 function port_definitions.get_ports(entity)
-    local def = port_definitions[entity.name]
-    if not def then return nil end
-    return def.ports or (def.by_direction and def.by_direction[entity.direction])
+    return port_definitions[entity.name][entity.direction]
 end
 
 return port_definitions

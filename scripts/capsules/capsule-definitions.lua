00421af140dfe8d5a46dbb2f7d93ced3ab537e5d
Pretzelcashew <hobermallowfoundationseason2@gmail.com>
2026-08-21 12:39:07 -0400

    added uhb and capsule definitions

---
+++ scripts/capsules/capsule-definitions.lua
@@ -0,0 +1,13 @@
+local capsule_definitions = {}
+
+capsule_definitions.types = {
+    ["item-capsule"] = {
+        type = "item-capsule",
+        base_capacity = 1,
+        mixed_cargo = true,
+        minimum_cargo = 1,
+        include_self = true
+    }
+}
+
+return capsule_definitions
---
+++ scripts/hubs/hub-definitions.lua
@@ -0,0 +1,12 @@
+local hub_definitions = {}
+
+hub_definitions.types = {
+    ["capsule-hub-horizontal"] = {
+        type = "hub"
+    },
+    ["capsule-hub-vertical"] = {
+        type = "hub"
+    }
+}
+
+return hub_definitions
